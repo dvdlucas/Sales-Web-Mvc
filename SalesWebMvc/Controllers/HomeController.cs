3316582c8f265a66deb27cee1adaddb2124cab1d
David Lucas <davidlucaskl@gmail.com>
2023-07-05 10:33:26 -0300

    Firt controller and Razor pages test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -27,6 +27,13 @@ namespace SalesWebMvc.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web MVC App From C# Course";
+            ViewData["email"] = "Davidlucaskl@gmail.com";
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
