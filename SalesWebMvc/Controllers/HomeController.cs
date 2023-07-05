bceb79ed57da3d444810c4a12ae1a343d88202b5
David Lucas <davidlucaskl@gmail.com>
2023-07-05 11:35:30 -0300

    Firt model-controller view Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
